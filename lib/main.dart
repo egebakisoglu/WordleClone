52309ee08f0902c769ad84e8be04bddaf888fed5
egebakisoglu <egebakisoglu@gmail.com>
2023-09-16 19:54:15 +0300

    changed font family to google fonts kanit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wordle/screens/wordle_screen.dart';
 
 void main() {
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(),
+      ),
       title: 'Flutter Demo',
       home: WordleScreen(),
     );
